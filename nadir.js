556baedea69516c60cab13199aab0e9bd4e37dfa
trhi <terhim@gmail.com>
2020-11-27 17:20:49 +0000

    Simplify: reveal on mouse move, limit dimension of image
    Simply move the mouse over the image to reveal it. Increase the size of the circle in order to reveal faster. Set alpha of revealer to 255 to reveal faster. Limit dimension of image to height 700px, to more or less fill the screen.

--- nadir.js
+++ nadir.js
@@ -1,10 +1,5 @@
-//Use mouse to "erase" and make visible the painting of Nadir afonso
-//Add audio cues: when the mouse is dragged over certain spots,
-//audio is triggered
-//
+//Use mouse to "erase" and make visible the paintings of Nadir afonso
 
-
-let img;
 let images = ["images/guaches-nadir-cortado-1.jpg",
 "images/guaches-nadir-cortado-39L.jpg",
 "images/guaches-nadir-cortado-581.jpg",
@@ -12,78 +7,28 @@ let images = ["images/guaches-nadir-cortado-1.jpg",
 "images/guaches-nadir-cortado-1773.jpg",
 ]
 let canvas;
+var canvasX = window.innerWidth;
+var canvasY = window.innerHeight;
 var erasing;
-//var eraserColor = color(255,255,255,127);
-var eraserSize = 60;
-
-function preload(){
- //img = loadImage('images/guaches-nadir-cortado-1.jpg');
-
-
-}
+var eraserSize = 100;
 
 function setup(){
-
-  img = createImg(random(images));
-
-  //img = createImg("images/guaches-nadir-cortado-1.jpg");
-
-  img.position(0, 0);
-  img.size(1250, 850);
-
-  canvas = createCanvas(1250, 850);
+  var img = createImg(images[0]);
+  img.style("max-height", "700px");
+  noStroke();
+  canvas = createCanvas(canvasX, canvasY);
   canvas.position(0, 0);
-
-
-  //background(153);
-  //image(img, 0, 0);
   fill(255,255,255,255);
   rect(0,0,1250,850);
 }
 
-
-
 function draw(){
-  //background(img);
-
-  //fill(0,0,0,0);
-  //ellipse(mouseX, mouseY, 40,40);
-}
-
-function mousePressed(){
   erasing = true;
-  //fill("black");
-  //text("pressed", mouseX+5, mouseY+5);
-  var eraser = color(255,255,255,100);
-  //eraser.setAlpha(0);
+  var eraser = color(255,255,255,255);
   erase();
   noStroke();
   fill(eraser);
+  //move mouse to reveal:
   ellipse(mouseX, mouseY, eraserSize, eraserSize);
   noErase();
-  //keyPressed();
-}
-
-function mouseDragged() {
-  //erasing = true;
-  if (erasing) {
-    //bx = mouseX - xOffset;
-    //by = mouseY - yOffset;
-  }
-
-  //fill("black");
-  //text("dragging", mouseX+5, mouseY+5);
-
-  erase();
-  var eraser = color(255,255,255,100);
-  noStroke();
-  fill(eraser);
-  ellipse(mouseX, mouseY, eraserSize, eraserSize);
-  noErase();
-}
-
-function mouseReleased() {
-  erasing = false;
-  //fill("black");
-  //text("released",mouseX+5, mouseY+5);
 }
